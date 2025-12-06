90b2ac486f043e0940c6319a085c769b31a8d6ef
Jaswanth <107982062+Adijaswanth@users.noreply.github.com>
2025-12-06 16:04:16 +0530

    Added ContactService microservice project

---
+++ ContactService/src/main/java/com/example/service/ContactFeignClient.java
@@ -0,0 +1,17 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.beans.Contact;
+
+@FeignClient(name = "contact-ms")
+public interface ContactFeignClient {
+
+    @GetMapping("/contacts/{userId}")
+    List<Contact> getSpecificContact(@PathVariable("userId") int userId);
+
+}
